53f5a432122807967b7e17d469e4b3ced823f844
Braden C <A02401510@aggies.usu.edu>
2026-02-21 14:00:38 -0700

    created application and started supabase integration

---
+++ utils/supabase/server.ts
@@ -0,0 +1,25 @@
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export async function createSupabaseServerClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+  return createServerClient(supabaseUrl, supabaseAnonKey, {
+    cookies: {
+      async getAll() {
+        return (await cookies()).getAll()
+      },
+      async setAll(cookiesToSet) {
+        try {
+        const cookieStore = await cookies()
+          cookiesToSet.forEach(({ name, value, options }) => {
+            cookieStore.set(name, value, options)
+          })
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    }
+  })
+}
